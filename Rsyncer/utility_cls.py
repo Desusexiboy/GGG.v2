eeb42cf367eca7aeb5915b0773c65ece7d6b0ce8
AlexShpil <alexshpil@gmail.com>
2017-04-07 11:29:26 +0300

    Update utility_cls.py
    Latest update

--- Rsyncer/utility_cls.py
+++ Rsyncer/utility_cls.py
@@ -1,4 +1,6 @@
 # utility
+import os
+
 
 class Utility:
     @staticmethod
@@ -21,26 +23,40 @@ class Utility:
         """ Add '-p port' to a -e params, if port exist """
         if (port):
             index = 0
-            key_str = '-e \'ssh -p {}\''.format(port)
+            key_str = '-e ssh -p {}'.format(port)
             for item in Utility.gen(keys_list):
                 if (item.startswith('-e')):
                     index = keys_list.index(item)
                     keys_list[index] = key_str
                     break
             if (not index):
-                keys_list.append(key_str)
+                keys_list.insert(0, key_str)
 
         return keys_list
 
     @staticmethod
     def rsync_all(data_dict):
+        response_list = list()
         for item in Utility.gen(data_dict['client']):
             keys = list()
             keys.extend(data_dict['keys'])
 
-            item.pinger()
-            keys = Utility.port_to_keys(keys, item.port)
-            item.rsync_cmd(keys, data_dict['host_files'])
+            connection_error = item.pinger()
+            if (not connection_error):
+                keys = Utility.port_to_keys(keys, item.port)
+                response = item.try_rsync_cmd(keys, data_dict['host_files'])
+                response_list.append(response)
+            else:
+                response_list.append(connection_error)
+
+        return response_list
+
+    @staticmethod
+    def print_responses(response_list):
+        logger = Utility.rsynclog.logger_init('print_response')
+        for index, item in enumerate(Utility.gen(response_list)):
+            print (item)
+            Utility.rsynclog.debug_log(logger,item)
 
             #########################Logger section#########################
 
@@ -57,7 +73,8 @@ class Utility:
             logger.setLevel(logging.INFO)
             logger.setLevel(logging.DEBUG)
             handler = logging.FileHandler('rsyncer.log')
-            formatter = logging.Formatter('[%(asctime)s] - %(name)11s - %(levelname)6s : %(message)s',datefmt='%d-%m-%y %H:%M')
+            formatter = logging.Formatter('[%(asctime)s] - %(name)11s - %(levelname)6s : %(message)s',
+                                          datefmt='%d-%m-%y %H:%M')
             handler.setFormatter(formatter)
             logger.addHandler(handler)
             return logger
@@ -71,19 +88,20 @@ class Utility:
         def debug_log(logger, infostr):
             ''' Log debug message '''
             logger.debug(infostr)
+            #########################Helper section#########################
 
     class helper:
         '''Helper object will raise help messages'''
+
         @staticmethod
         def usage_help():
-            print('usage: '
-            'rsyncer.py [/dir file1 file2][-process][-e ssh][username:port@ip:/destination [-pass=PASS]][-PavSzq]\n'
-            'Runs rsync application with input parameters. For more info go to main_help.')
-
-
+            print('\n   usage:   '
+                  'rsyncer.py [/dir file1 file2][-process][-e ssh][username:port@ip:/destination [-pass=PASS]][-PavSzq]\n\n'
+                  'Runs rsync application with input parameters. For more info use "rsyncer.py -h".\n')
 
         @staticmethod
         def main_help():
+            Utility.helper.usage_help()
             print('All available keys:\n'
                   '-process                    If exists raise -process flag for rsync\n'
                   '-pass=[Password]            Password for connection to remote host\n'
@@ -105,6 +123,17 @@ class Utility:
                   'Valid separators between username and port are: comma, spot, colon (,.:)\n'
                   'Examples: rsyncer.py /usr root@host\n'
                   '          rsyncer.py /usr/wildcard* file3.avi root,22@hostname:/junk')
+
+        @staticmethod
+        def error_msg(logger, err_msg, info_msg='', exitcode=1):
+            print ('\n### {} :<').format(info_msg)
+            if (info_msg):
+                Utility.rsynclog.info_log(logger, info_msg)
+            if (err_msg):
+                Utility.rsynclog.debug_log(logger, err_msg)
+            Utility.helper.usage_help()
+            if (exitcode):
+                exit(1)
 
         @staticmethod
         def connection_type_help():
